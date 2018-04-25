39fb38b5846460e9be61f4823c0c6312ab8f4c46
Gregory Norton <freezedown@hotmail.com>
2018-04-24 23:11:16 -0400

    Removed compiled code and added more gitignores

--- rust/target/debug/cc6809.d
+++
@@ -1,1 +0,0 @@
-/home/ubuntu/Desktop/git/cc6809/rust/target/debug/cc6809: /home/ubuntu/Desktop/git/cc6809/rust/src/main.rs
--- rust/target/debug/deps/cc6809-7d3a3c8367b96b7e.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu/Desktop/git/cc6809/rust/target/debug/deps/cc6809-7d3a3c8367b96b7e: src/main.rs
-
-/home/ubuntu/Desktop/git/cc6809/rust/target/debug/deps/cc6809-7d3a3c8367b96b7e.d: src/main.rs
-
-src/main.rs:
