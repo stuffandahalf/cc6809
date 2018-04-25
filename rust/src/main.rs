6d62e15ff02abcee1e4ebbf066ff88b822fad677
Gregory Norton <freezedown@hotmail.com>
2018-04-24 23:09:14 -0400

    Started some rust stuff

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,29 @@
+/* Exit function */
+use std::process::exit;
+/* Environment variables */
+use std::env;
+/* File IO */
+use std::io::prelude::*;
+use std::fs::File;
+
+macro_rules! error {
+    ($cond:expr, $msg:expr) => {
+        if $cond {
+            eprintln!($msg);
+            exit(1);
+        }
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    error!(args.len() < 2, "Syntax:\t./cc6809 fname.c");
+    
+    let mut cfile: File;
+    match File::open(&*args[1]) {
+        Ok(file) => cfile = file,
+        Err(err) => error!(true, "Failed to open file"),
+    }
+    //println!("{:?}", cfile);
+    //let mut cfile = (File::open(&*args[1]))?;
 }
---
+++ rust/target/debug/cc6809.d
@@ -0,0 +1,1 @@
+/home/ubuntu/Desktop/git/cc6809/rust/target/debug/cc6809: /home/ubuntu/Desktop/git/cc6809/rust/src/main.rs
