87d05102d7c2b9ca20548a47026564f2ab10901b
Gregory Norton <gregory.norton@me.com>
2018-04-28 05:09:49 -0400

    Added missing files

--- sample.c
+++ sample.c
@@ -3,10 +3,12 @@
 
 int main (int argc, char **argv)
 {
+    #include "test.inc"
     //int x = 5;
     int x = TEST;
     int y = 5;
     int z = x + y;
+    //printf("%d\n", z);
     return 0;
 }
 
